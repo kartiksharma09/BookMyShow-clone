219dae774ce32c412ca02445e1b65ef84d72f8c7
kartik sharma <kartik19@navgurukul.org>
2021-08-10 21:38:48 +0530

    -

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with kartik sharma
+          Learn React with kartik
         </a>
       </header>
     </div>

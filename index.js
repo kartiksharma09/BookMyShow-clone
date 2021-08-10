2edbf717608261ee229e5082776829ba38bac26d
sonushakya9717 <sonu19@navgurukul.org>
2021-08-10 23:05:23 +0530

    written tests for admin-login file in controller

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const PORT = 5000;
 // // coneect your DB KRNA
 connectDB();
 
+app.use(express.json({ extended: false }))
+
+
 const user = require('./routes/users')
 
 // const admin = require('./routes/admin')
--- tests/src/controller/main.test.js
+++ tests/src/controller/main.test.js
@@ -2,6 +2,7 @@ const {connect,close,clear} = require('../../../config/testDb')
 const User = require("../../../models/Users");
 
 const {createUserTests, loginUserTests} = require('./users')
+const { adminLoginTests } = require("./admin-login")
 
 
 beforeAll(async () => await connect())
@@ -13,4 +14,6 @@ describe("Test suite for full backend",()=>{
     createUserTests()
 
     loginUserTests()
+
+    adminLoginTests()
 })

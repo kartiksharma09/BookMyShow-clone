689f3d5885856af6e35b6944f33e8fcd4f4f2191
rani-solanki <rani19@navgurukul.org>
2021-08-10 18:37:36 +0530

    added tickets model

--- models/tickets.js
+++ models/tickets.js
@@ -1,42 +1,42 @@
 const mongoose = require("mongoose");
 const tickeSchema = new mongoose.Schema({
-        cinemaId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "",
-        },
-        movieId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "",
-        },
-        Seats: {
-            type: Array,
-            required: true,
-        },
-        watchers: [
-            {
-                name: {
-                    type: "String",
-                    required: true,
-                },
-                age: {
-                    type: Number,
-                    required: true,
-                },
-                gender: {
-                    type: String,
-                    required: true,
-                },
-            },
-        ],
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "user",
-        },
-        bookingDate: {
-            type: Date,
-            required: true,
-        },
+    cinemaId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "cinema",
     },
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie",
+    },
+    Seats: [{
+        type: String,
+        required: true
+    }],
+    watchers: [
+        {
+            name: {
+                type: "String",
+                required: true,
+            },
+            age: {
+                type: Number,
+                required: true,
+            },
+            gender: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
+    bookingDate: {
+        type: Date,
+        required: true,
+    },
+},
     {
         timestamps: true,
     }

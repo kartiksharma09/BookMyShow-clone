b48bc28a3d567cb5f49142c8ba7cfd8cd92891e9
rani-solanki <rani19@navgurukul.org>
2021-08-10 18:23:04 +0530

    First commit

---
+++ models/tickets.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const tickeSchema = new mongoose.Schema({
+        cinemaId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "",
+        },
+        movieId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "",
+        },
+        Seats: {
+            type: Array,
+            required: true,
+        },
+        watchers: [
+            {
+                name: {
+                    type: "String",
+                    required: true,
+                },
+                age: {
+                    type: Number,
+                    required: true,
+                },
+                gender: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+        },
+        bookingDate: {
+            type: Date,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+const tickets = mongoose.model("tickets", tickeSchema);
+module.exports = tickets;

4a9697e2a54da0af4a6293cd7d30ae8d9c81926b
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-08-10 17:13:59 +0530

    deleted models/cinema.js

--- models/Users.js
+++ models/Users.js
@@ -15,15 +15,15 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
     isAdmin: {
-        type:Boolean,
-        default:false
+        type: Boolean,
+        default: false
     },
     date: {
         type: Date,
         default: Date.now
     }
-})
+});
 
-const User = mongoose.model('user', UserSchema);
+const Users = mongoose.model('user', UserSchema);
 
 module.exports = Users;

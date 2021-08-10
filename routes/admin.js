921b3e5196958404ec017c8e191860ddd968f6aa
ROHIT <rohit19@navgurukul.org>
2021-08-11 00:58:00 +0530

    changed something

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,26 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const cinema=require('../controllers/cinema')
 const {adminLogin}=require("../controllers/admin-login")
+const auth=require('../middleware/auth')
+const isAdmin=require('../middleware/isAdmin')
 
 
 router.post("/admin/Adminlogin",[
     check("email", "please include a valid email").isEmail(),
     check("password", "password is required").exists(),
 ],adminLogin)
+
+
+
+router.post("/admin/addCinema",[auth, isAdmin,
+    [check("cinemaName","cinema hall name is required")   .not()
+    .isEmpty(),
+    check("locationOdCinema","cinema location is required")   .not()
+    .isEmpty(),
+    check("seats","seats is required").not()
+    .isEmpty(),,
+    check("Movies","movies is required")   .not()
+    .isEmpty()]
+    ],cinema)
 
 module.exports = router;

01530fe4ac17a6d915b103c7fd809f7d529d5de4
ROHIT <rohit19@navgurukul.org>
2021-08-10 15:21:32 +0530

    mongo connected

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const connectDB=require('./config/db')
+
 
 const PORT = 5000;
 
+// coneect your DB KRNA
+connectDB();
+
 app.get("/api", (req, res) => {
-    res.send("hello coders");
+    res.send("hello coders....");
 });
 
 app.listen(PORT, () => {

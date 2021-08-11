153b453b6d29b2f840471d5e0376170e769441fb
ROHIT <rohit19@navgurukul.org>
2021-08-11 15:41:58 +0530

    react intialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './component/layout/navbar'
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar/>
+        <Navbar/>
       </Router>
     </div>
   );

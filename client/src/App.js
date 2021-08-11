532824dc3b58150c9dc3b7e0667910da70968681
sonushakya9717 <sonu19@navgurukul.org>
2021-08-11 23:13:27 +0530

    done with login and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Navbar from './component/layout/navbar'
+import AdminRegister from "./component/admin-auth/AdminRegister"
+import AdminLogin from './component/admin-auth/AdminLogin';
+import UserLogin from './component/user-auth/UserLogin';
+import UserSignup from './component/user-auth/UserSignup';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,7 +16,11 @@ function App() {
     
     <div className="App">
       <Router>
-        <Navbar/>
+      <Route exact path='/admin-register' component={AdminRegister} />
+      <Route exact path='/admin-login' component={AdminLogin} />
+      <Route exact path='/user-login' component={UserLogin} />
+      <Route exact path='/user-register' component={UserSignup} />
+        {/* <Navbar/> */}
       </Router>
     </div>
     

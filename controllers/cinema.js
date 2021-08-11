7c818688a337a8b7626af43230c79d697ff20f89
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-08-11 18:13:44 +0530

    modified controller/cinema

--- controllers/cinema.js
+++ controllers/cinema.js
@@ -1,34 +1,34 @@
-const Cinema=require('../models/cinema')
-const User=require('../models/Users')
-const {validationResult}=require('express-validator')
+const Cinema = require('../models/cinema');
+const User = require('../models/Users');
+const { validationResult } = require('express-validator');
 
-const cinema=async(req,res)=>{
-    console.log(req.body,"djkjd")
+const cinema = async(req, res) => {
+    console.log(req.body, "djkjd");
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log(errors)
-      return res.status(400).json({ msg:"checking"});
+        console.log(errors)
+        return res.status(400).json({ msg: "checking" });
     }
 
-    const cinemas=new Cinema(req.body)
+    const cinemas = new Cinema(req.body);
 
-    console.log(cinemas)
+    console.log(cinemas);
     try {
-       
 
-        cinemas.adminId=req.user.id
-        
-        await cinemas.save()
-        res.status(200).json({msg:"your cinemaHall is added..."})
-        
-       
-       
+
+        cinemas.adminId = req.user.id;
+
+        await cinemas.save();
+        res.status(200).json({ msg: "your cinemaHall is added..." });
+
+
+
     } catch (err) {
         console.log(err)
-        res.status(500).json({ msg: "server error....." })
+        res.status(500).json({ msg: "server error....." });
     }
 }
 
 
 
-module.exports={cinema}
+module.exports = { cinema };

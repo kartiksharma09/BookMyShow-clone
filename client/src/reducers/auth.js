5555350321f3e1b7e0b2f2e3a5665aae6c030832
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-08-13 00:14:03 +0530

    added getAllmovies action

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,56 +1,57 @@
-import {REGISTER_FAIL,
-    REGISTER_SUCCESS,
-    AUTH_ERROR,
-    USER_LOADED,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT,
-    ACCOUNT_DELETED,
-    ADMIN_CALLED
-} from "../actions/types";
+// import {
+//     REGISTER_FAIL,
+//     REGISTER_SUCCESS,
+//     AUTH_ERROR,
+//     USER_LOADED,
+//     LOGIN_SUCCESS,
+//     LOGIN_FAIL,
+//     LOGOUT,
+//     ACCOUNT_DELETED,
+//     ADMIN_CALLED
+// } from "../actions/types";
 
-const initialState = {
-    token : localStorage.getItem('token'),
-    isAuthenticated : false ,
-    loading: true,
-    user : null
-};
+// const initialState = {
+//     token: localStorage.getItem('token'),
+//     isAuthenticated: false,
+//     loading: true,
+//     user: null
+// };
 
-const auth=(state = initialState, action)=>{
-    const {type, payload} = action;
+// const auth = (state = initialState, action) => {
+//     const { type, payload } = action;
 
-    switch(type){
-        case USER_LOADED:
-            return{
-                ...state,
-                isAuthenticated:true,
-                loading:false,
-                user:payload
-            }
-        case REGISTER_SUCCESS:
-        case LOGIN_SUCCESS:
-            localStorage.setItem('token',payload.token);
-            return{
-                ...state,
-                ...payload,
-                isAuthenticated:true,
-                loading:false
-            }
-        case REGISTER_FAIL:
-        case AUTH_ERROR:
-        case LOGIN_FAIL:
-        case LOGOUT:
-        case ACCOUNT_DELETED:
-            localStorage.removeItem('token');
-            return{
-                ...state,
-                token:null,
-                isAuthenticated:false,
-                loading:false
-            }
-        default:
-            return state;
-    }
-}
+//     switch (type) {
+//         case USER_LOADED:
+//             return {
+//                 ...state,
+//                 isAuthenticated: true,
+//                 loading: false,
+//                 user: payload
+//             }
+//         case REGISTER_SUCCESS:
+//         case LOGIN_SUCCESS:
+//             localStorage.setItem('token', payload.token);
+//             return {
+//                 ...state,
+//                 ...payload,
+//                 isAuthenticated: true,
+//                 loading: false
+//             }
+//         case REGISTER_FAIL:
+//         case AUTH_ERROR:
+//         case LOGIN_FAIL:
+//         case LOGOUT:
+//         case ACCOUNT_DELETED:
+//             localStorage.removeItem('token');
+//             return {
+//                 ...state,
+//                 token: null,
+//                 isAuthenticated: false,
+//                 loading: false
+//             }
+//         default:
+//             return state;
+//     }
+// }
 
-export default auth;
+// export default auth;
---
+++ client/src/reducers/movies.js
@@ -0,0 +1,15 @@
+import {
+    SEARCH_MOVIES,
+    SEARCH_MOVIES_FAIL
+} from '../actions/movies';
+
+
+initialState = {
+    movies: [],
+    loading: true
+};
+
+
+const movies = (state = initialState, action) => {
+
+}

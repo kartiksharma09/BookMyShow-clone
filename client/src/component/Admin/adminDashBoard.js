3a67b870f4de69475e536e7d6b9eb6ec4ec0e4ac
sonushakya9717 <sonu19@navgurukul.org>
2021-08-13 01:08:46 +0530

    add movie api integration

--- client/src/component/Admin/adminDashBoard.js
+++ client/src/component/Admin/adminDashBoard.js
@@ -15,50 +15,47 @@ export const AdminDashBoard = () => {
                 role='tablist'
                 aria-orientation='vertical'
               >
-                <a
+                <Link
                   className='nav-link active btn btn-danger my-1'
                   id='v-pills-home-tab'
                   data-toggle='pill'
-                  href='#v-pills-home'
                   role='tab'
                   aria-controls='v-pills-home'
                   aria-selected='true'
                 >
                   Home
-                </a>
-                <a
+                </Link>
+                <Link
+                to="/addMovie"
                   className='nav-link btn btn-danger my-1'
                   id='v-pills-profile-tab'
                   data-toggle='pill'
-                  href='#v-pills-profile'
                   role='tab'
                   aria-controls='v-pills-profile'
                   aria-selected='false'
                 >
                   AddMovie
-                </a>
-                <a
+                </Link>
+                <Link
                   className='nav-link btn btn-danger my-1'
                   id='v-pills-messages-tab'
                   data-toggle='pill'
-                  href='#v-pills-messages'
                   role='tab'
                   aria-controls='v-pills-messages'
                   aria-selected='false'
                 >
                   AddCinema
-                </a>
-                <a
+                </Link>
+                <Link
                   className='nav-link btn btn-danger my-1'
                   id='v-pills-settings-tab'
                   data-toggle='pill'
-                  href='#v-pills-settings'
                   role='tab'
                   aria-controls='v-pills-settings'
                   aria-selected='false'
                 >
                   Delete your Agency
-                </a>
+                </Link>
               </div>
             </div>
             <div className='col-9'>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import alert from './alert';
 import auth from './auth';
+import movie from './movie';
 
 export default combineReducers({
     alert,
-    auth
+    auth,
+    movie
 });

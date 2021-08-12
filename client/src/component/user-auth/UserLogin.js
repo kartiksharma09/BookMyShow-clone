1ed5d25c393b0a5c07bafc57c95d4fb9bb34ee7b
ROHIT <rohit19@navgurukul.org>
2021-08-12 18:16:56 +0530

    userlogin again

--- client/src/component/user-auth/UserLogin.js
+++ client/src/component/user-auth/UserLogin.js
@@ -1,52 +1,36 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import '../../css/user-auth.scss';
+import '../../css/form.css';
 
 const UserLogin = () => {
   return (
     <Fragment>
-      <div class='panda'>
-        <div class='ear'></div>
-        <div class='face'>
-          <div class='eye-shade'></div>
-          <div class='eye-white'>
-            <div class='eye-ball'></div>
-          </div>
-          <div class='eye-shade rgt'></div>
-          <div class='eye-white rgt'>
-            <div class='eye-ball'></div>
-          </div>
-          <div class='nose'></div>
-          <div class='mouth'></div>
-        </div>
-        <div class='body'> </div>
-        <div class='foot'>
-          <div class='finger'></div>
-        </div>
-        <div class='foot rgt'>
-          <div class='finger'></div>
+      <div className='col-12 forms-row'>
+        <div class='row p-4 h-100 justify-content-center align-items-center'>
+          <form className='login'>
+            <h3 className='text-center'>Log In</h3>
+            <input
+              className='form-inputs'
+              type='email'
+              placeholder='Email'
+              required
+            />
+            <input
+              className='form-inputs'
+              type='password'
+              placeholder='Password'
+              required
+            />
+            <p className='form-submit text-center btn btn-danger'>LOGIN</p>
+            <p className='text-white'>
+              Don't have an account?{' '}
+              <Link className='text-warning' to='/admin-register'>
+                SignUp
+              </Link>
+            </p>
+          </form>
         </div>
       </div>
-      <form>
-        <div class='hand'></div>
-        <div class='hand rgt'></div>
-        <h1>Panda Login</h1>
-        <div class='form-group'>
-          <input required='required' class='form-control' />
-          <label class='form-label'>Username </label>
-        </div>
-        <div class='form-group'>
-          <input
-            id='password'
-            type='password'
-            required='required'
-            class='form-control'
-          />
-          <label class='form-label'>Password</label>
-          <p class='alert'>Invalid Credentials..!!</p>
-          <button class='btn'>Login </button>
-        </div>
-      </form>
     </Fragment>
   );
 };
